3a779914e21f67c3cf054968b60329fb3b439c13
Merkurev Maxim <96414688+kos94ok@users.noreply.github.com>
2022-08-31 16:06:08 +0500

    Fix error with resize label

--- models/head/ct_head.py
+++ models/head/ct_head.py
@@ -93,7 +93,7 @@ class CT_Head(nn.Module):
         img_size = img_meta['img_size'][0]
 
         label_num = np.max(label) + 1
-        label = cv2.resize(label, (img_size[1], img_size[0]), interpolation=cv2.INTER_NEAREST)
+        label = cv2.resize(label, (int(np.float32(img_size[1])), int(np.float32(img_size[0]))), interpolation=cv2.INTER_NEAREST)
 
         if not self.training and cfg.report_speed:
             torch.cuda.synchronize()
